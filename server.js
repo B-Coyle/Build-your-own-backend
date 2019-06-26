0636ead46cd1cbd57427412c4414763d761b65dd
Bridgett Coyle <bacoyle0409@gmail.com>
2019-06-26 15:54:56 -0600

    Add tentative get for books,book:id,additional, and additional:id

--- server.js
+++ server.js
@@ -10,12 +10,12 @@ const port  = 3000;
 app.use(express.json());
 app.use(cors());
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log(`App is running on port ${port}`)
 })
 
 app.get('/api/v1/books', (request, response) => {
-    database('summerread').select()
+    database('books').select()
     .then((books) => {
         response.status(200).json(books);
     })
@@ -24,13 +24,32 @@ app.get('/api/v1/books', (request, response) => {
     })
 });
 
-//update this get because it's not targeting the id specifically
-app.get('./api/v1/books:id', (request, response) => {
-    database('summerread').select()
-    .then((books) => {
-        response.status(200).json(books);
+app.get('./api/v1/books/:id', (request, response) => {
+    database('books').select()
+    .then(books => {
+        const id = parseInt(req.params.id);
+        const found = books.find(book => book.id === id)
+        response.status(200).json(found);
+    })
+    .catch(error => response.status(500).json({error}))
+})
+
+app.get('./api/v1/additional', (request, response) => {
+    database('additional').select()
+    .then((additional) => {
+        response.status(200).json(additional);
     })
     .catch((error) =>{
         response.status(500).json({error})
     })
+})
+
+app.get('./api/v1/additional/:id', (request,response) => {
+    database('additional').select()
+    .then(additional => {
+        const id = parseInt(req.params.id);
+        const found = additional.find(info => info.id === id);
+        response.status(200).json(found);
+    })
+    .catch(error => response.status(500).json({error}))
 })
