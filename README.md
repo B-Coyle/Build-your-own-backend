19d255b783aeffccf28079cf9f8bb4abb5364aab
Bridgett Coyle <bacoyle0409@gmail.com>
2019-06-26 18:15:30 -0600

    Add tentative readme

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
-# Build-your-own-backend
+# Build-your-own-backend-Summer Reading List
+
+Summer is on us at last! Time to catch up on some vitamin D and the giant pile of books you've been telling yourself you'd get to eventually. Don't have a giant pile calling your name (especially at the bookstore when you buy yet another book)? That's okay! Because the Summer Reading List provides a backend of the 30 most popular Sci-fi/Fantasy books (if that genre is your thing). So pull up comfy chair and get to reading.
+
+\*\*Feel free to contribue by adding your own books that you want to read.
+
+##API Calls
+
+##Get
+
+You can get data on several books with their authors, descriptions, total page length, and average list price.
+
+##GET /api/v1/books
+
+Sample Response
+id:157,
+title: The Lord Of The Rings Trilogy,
+author: J.R.R. Tolkien,
+description: 	Tolkien's seminal three-volume epic chronicles the War of the Ring, in which Frodo the hobbit and his companions set out to destroy the evil Ring of Power and restore peace to Middle-earth. The beloved trilogy still casts a long shadow, having established some of the most familiar and enduring tropes in fantasy literature.,
+created_at: 2019-06-26 17:03:24.739759-06,	
+updated_at: 2019-06-26 17:03:24.739759-06
+id: 158,
+title: The Hitchhiker's Guide To The Galaxy,
+author: Douglas Adams,
+description: In the first, hilarious volume of Adams' Hitchhiker's series, reluctant galactic traveler Arthur Dent gets swept up in some literally Earth-shattering events involving aliens, sperm whales, a depressed robot, mice who are more than they seem, and some really, really bad poetry.	
+created_at: 2019-06-26 17:03:24.74631-06,	
+updated_at: 2019-06-26 17:03:24.74631-06
+
+id: 159,
+title:	The Dune Chronicles,
+author: Frank Herbert,
+description:	Follows the adventures of Paul Atreides, the son of a betrayed duke given up for dead on a treacherous desert planet and adopted by its fierce, nomadic people, who help him unravel his most unexpected destiny.,
+created_at:	2019-06-26 17:03:24.753956-06,
+updated_at:	2019-06-26 17:03:24.753956-06
+
+
+##GET /api/v1/additional
+
+additional: {
+id: 1,
+pages: 216,
+ list_price: 6.99}},
--- server.js
+++ server.js
@@ -1,55 +1,112 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
-const environment = process.env.NODE_ENV || 'development';
-const configuration = require('./knexfile')[environment];
-const database = require('knex')(configuration)
-const port  = 3000;
-
+const environment = process.env.NODE_ENV || "development";
+const configuration = require("./knexfile")[environment];
+const database = require("knex")(configuration);
+const port = 3000;
 
 app.use(express.json());
 app.use(cors());
 
 app.listen(port, () => {
-    console.log(`App is running on port ${port}`)
-})
-
-app.get('/api/v1/books', (request, response) => {
-    database('books').select()
-    .then((books) => {
-        response.status(200).json(books);
-    })
-    .catch((error) => {
-        response.status(500).json({error})
-    })
+  console.log(`App is running on port ${port}`);
 });
 
-app.get('./api/v1/books/:id', (request, response) => {
-    database('books').select()
+app.get("/api/v1/books", (request, response) => {
+  database("books")
+    .select()
     .then(books => {
-        const id = parseInt(req.params.id);
-        const found = books.find(book => book.id === id)
-        response.status(200).json(found);
+      response.status(200).json(books);
     })
-    .catch(error => response.status(500).json({error}))
-})
+    .catch(error => {
+      response.status(500).json({ error });
+    });
+});
 
-app.get('./api/v1/additional', (request, response) => {
-    database('additional').select()
-    .then((additional) => {
-        response.status(200).json(additional);
+app.get("./api/v1/books/:id", (request, response) => {
+  database("books")
+    .select()
+    .then(books => {
+      const id = parseInt(req.params.id);
+      const found = books.find(book => book.id === id);
+      response.status(200).json(found);
     })
-    .catch((error) =>{
-        response.status(500).json({error})
-    })
-})
+    .catch(error => response.status(500).json({ error }));
+});
 
-app.get('./api/v1/additional/:id', (request,response) => {
-    database('additional').select()
+app.get("./api/v1/additional", (request, response) => {
+  database("additional")
+    .select()
     .then(additional => {
-        const id = parseInt(req.params.id);
-        const found = additional.find(info => info.id === id);
-        response.status(200).json(found);
+      response.status(200).json(additional);
     })
-    .catch(error => response.status(500).json({error}))
-})
+    .catch(error => {
+      response.status(500).json({ error });
+    });
+});
+
+app.get("./api/v1/additional/:id", (request, response) => {
+  database("additional")
+    .select()
+    .then(additional => {
+      const id = parseInt(req.params.id);
+      const found = additional.find(info => info.id === id);
+      response.status(200).json(found);
+    })
+    .catch(error => response.status(500).json({ error }));
+});
+
+app.post("./api/v1/books", (request, response) => {
+  const newBook = request.body;
+  const format = ["title", "author", "description"];
+  for (let requiredParam of format) {
+    if (!newBook[requiredParam] && newBook[requiredParam] !== "") {
+      response
+        .status(422)
+        .json({
+          error: `Expected form of ${format.join(
+            ", "
+          )}. Missing: ${requiredParam}.`
+        });
+    }
+  }
+  database("books")
+    .select()
+    .then(books => {
+      if (books.some(book => book.title === newBook.title)) {
+        response.status(500).json({ error: "Book already exists in list." });
+      }
+    });
+  database("books")
+    .insert(newBook, "id")
+    .then(book => response.status(201).json(book))
+    .catch(error => response.status(500).json({ error }));
+});
+
+app.post("./api/v1/additional", (request, response) => {
+  const newInfo = request.body;
+  const format = ["pages", "list_price"];
+  for (let requiredParam of format) {
+    if (!newInfo[requiredParam] && newInfo[requiredParam] !== "") {
+      response
+        .status(422)
+        .json({
+          error: `Expected form of ${format.join(
+            ", "
+          )}. Missing: ${requiredParam}.`
+        });
+    }
+  }
+  database("additional")
+    .select()
+    .then(books => {
+      if (info.some(info => info.pages === newInfo.pages)) {
+        response.status(500).json({ error: "Additional book information already exists in list." });
+      }
+    });
+  database("additional")
+    .insert(newInfo, "id")
+    .then(info => response.status(201).json(info))
+    .catch(error => response.status(500).json({ error }));
+});
