4bb2d2350aeaf2b20b9a0f3e7ae5dc4f3d3cfc8c
Bridgett Coyle <bacoyle0409@gmail.com>
2019-06-28 10:46:27 -0600

    Add set port

--- server.js
+++ server.js
@@ -3,7 +3,10 @@ const app = express();
 const environment = process.env.NODE_ENV || "development";
 const configuration = require("./knexfile")[environment];
 const database = require("knex")(configuration);
-const port = 3000;
+
+const port = process.env.PORT || 3000;
+
+app.set('port', process.env.PORT || 3000)
 
 app.use(express.json());
 
